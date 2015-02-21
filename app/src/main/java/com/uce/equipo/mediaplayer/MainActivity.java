a60af9f8ee115b655439b51192c3dcce1ecefa0e
Fabricio Gavilanes <Fabricio3220@hotmail.com>
2015-02-21 01:05:39 -0500

    Nombre del Boton

--- app/src/main/java/com/uce/equipo/mediaplayer/MainActivity.java
+++ app/src/main/java/com/uce/equipo/mediaplayer/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-
 public class MainActivity extends ActionBarActivity {
     public Button bplay,bstop,bsoundPool;
     public MediaPlayer mp;
